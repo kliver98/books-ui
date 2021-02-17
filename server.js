14cfcd45285e1857fa354c34d452bab28bfacd89
Kliver Daniel <kliver1998@gmail.com>
2021-02-17 00:15:50 -0500

    Added Cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //Install express server
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
+app.use(cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/books-ui'));
 
