419652b5848d7723f64e414767348f2f936b11f3
Kliver Daniel <kliver1998@gmail.com>
2021-03-17 21:00:05 -0500

    Testing environment varibale to call backend api

--- set-env.js
+++ set-env.js
@@ -12,6 +12,6 @@ writeFile(targetPath, envConfigFile, function (err) {
    if (err) {
        throw console.error(err);
    } else {
-       console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+       console.log(`Angular environment.ts file generated correctly at ${targetPath} \n${envConfigFile}`);
    }
 });
